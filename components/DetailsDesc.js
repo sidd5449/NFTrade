9afa7b0827a20c3265d8304bf30ff2e0a06a6854
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2023-01-24 00:16:44 +0530

    completed app

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -1,11 +1,67 @@
+import { useState } from 'react'
 import { View, Text } from 'react-native'
-import React from 'react'
+import { EthPrice, NFTTitle } from './SubInfo'
+import {COLORS, SIZES, FONTS} from '../constants'
 
-const DetailsDesc = () => {
+const DetailsDesc = ({ data }) => {
+  const [text, settext] = useState(data.description.slice(0, 100));
+  const [readmore, setreadmore] = useState(false);
   return (
-    <View>
-      <Text>DetailsDesc</Text>
+    <>
+    <View style={{
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    }}>
+      <NFTTitle 
+        title={data.name}
+        subTitle={data.creator}
+        titleSize={SIZES.extraLarge}
+        subTitleSize={SIZES.font} 
+      />
+      <EthPrice price={data.price}/>
     </View>
+    <View style={{marginVertical: SIZES.extraLarge*1.5}}>
+      <Text style={{
+        fontSize:SIZES.font,
+        fontFamily: FONTS.semiBold,
+        color: COLORS.primary
+      }}>Description</Text>
+      <View style={{marginTop: SIZES.base}}>
+        <Text style={{
+          fontSize:SIZES.small,
+          fontFamily: FONTS.regular,
+          color: COLORS.secondary,
+          lineHeight: SIZES.large
+        }}>{text}
+        {!readmore && '...'}
+        <Text style={{
+          fontSize:SIZES.small,
+          fontFamily: FONTS.semiBold,
+          color: COLORS.primary,
+        }}
+        onPress={() => {
+          if(!readmore){
+            settext(data.description)
+            setreadmore(true)
+          }
+          else{
+            settext(data.description.slice(0, 100));
+            setreadmore(false);
+          }
+        }}
+        >
+          {readmore ? 'Show Less': "Read More"}
+
+        </Text>
+        </Text>
+        
+      </View>
+
+    </View>
+    
+    </>
   )
 }
 
--- screens/Details.js
+++ screens/Details.js
@@ -67,6 +67,18 @@ const Details = ({ route, navigation }) => {
           <React.Fragment>
             <DetailsHeader data={data} navigation={navigation}/>
             <SubInfo />
+            <View style={{padding: SIZES.font}}>
+              <DetailsDesc data={data} />
+              {data.bids.length>0 && (
+                <Text style={{
+                  fontSize: SIZES.font,
+                  fontFamily: FONTS.semiBold,
+                  color: COLORS.primary
+                }}
+                >Current Bids</Text>
+              )}
+
+            </View>
           </React.Fragment>
         )}
       />
